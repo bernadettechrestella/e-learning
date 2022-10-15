333625986a314ef93ad530555a73657141fb953c
bernadettechrestella <bernadettechrestella@gmail.com>
2022-10-15 14:08:40 +0500

    add route Konten

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
+use App\Http\Controllers\Konten;
 
 Route::post('/tambahAdmin', [Admin::class, 'tambahAdmin']);
 Route::post('/loginAdmin', [Admin::class, 'loginAdmin']);
 Route::post('/hapusAdmin', [Admin::class, 'hapusAdmin']);
 Route::post('/listAdmin', [Admin::class, 'listAdmin']);
+
+Route::post('/tambahKonten', [Konten::class, 'tambahKonten']);
+Route::post('/ubahKonten', [Konten::class, 'ubahKonten']);
